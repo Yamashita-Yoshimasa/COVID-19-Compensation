8e3406591c6f3025afdea3761bf77cbd9021e933
Yamashita-Yoshimasa <yychbook040720@gmail.com>
2020-04-14 00:04:26 +0900

    ScrollToTop 追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React, { FC } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import Introduction from 'components/Home/Introduction';
 import Subsidy from 'components/SubsidyList/Subsidy';
+import ScrollToTop from 'containers/ScrollToTop';
 
 const App: FC<{}> = () => (
-  <Switch>
-    <Route exact path="/" component={Introduction} />
-    <Route exact path="/subsidy/:code" component={Subsidy} />
-    <Redirect to="/" />;
-  </Switch>
+  <>
+    <Switch>
+      <Route exact path="/" component={Introduction} />
+      <Route exact path="/subsidy/:code" component={Subsidy} />
+      <Redirect to="/" />;
+    </Switch>
+    <ScrollToTop />
+  </>
 );
 export default App;
