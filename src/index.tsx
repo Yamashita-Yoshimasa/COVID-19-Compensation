98382d955a245c6306eb3f3e5ea27fd6ee64501f
Yamashita-Yoshimasa <yychbook040720@gmail.com>
2020-04-16 22:14:59 +0900

    twitter card 実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from 'serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import App from 'App';
 import Header from 'components/layouts/Header';
 import Footer from 'components/layouts/Footer';
+import TwitterCard from 'images/card.png';
 
 ReactDOM.render(
   <BrowserRouter>
+    <Helmet>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@Tapi_tapirus" />
+      <meta property="og:url" content="https://covid-19.yamabaku.work/" />
+      <meta property="og:title" content="COVID-19 Subsidies" />
+      <meta
+        property="og:description"
+        content=" 新型コロナウイルスに対する国の制度を確認！"
+      />
+      <meta property="og:image" content={TwitterCard} />
+    </Helmet>
     <Header />
     <App />
     <Footer />
